a27b2aaf99aa38ffd4efdf14aa0cba285fb2d112
RickyMandich <ricky.mandich@gmail.com>
2025-01-17 18:01:51 +0100

    aggiornamento 2025 01 17 18:01

--- src/main/java/com/example/webscraping/Test.java
+++ src/main/java/com/example/webscraping/Test.java
@@ -9,32 +9,39 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
+    public static String extractCid(String url) {
+        String cid = "";
+        Pattern pattern = Pattern.compile("cid=([0-9]+)");
+        Matcher matcher = pattern.matcher(url);
+        if (matcher.find()) {
+            cid = matcher.group(1);
+        }
+        return cid;
+    }
+
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         try {
             driver.get("https://starwarsunlimited.com/it/cards");
             int i = 0;
             do{
-                if(i%1000==0) System.out.println("attento il caricamento della pagina...");
-                ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,10000);");
+                ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,1000);");
                 i++;
             }while(driver.findElement(By.cssSelector("body")).getText().toLowerCase().contains("carica"));
-            System.out.println(driver.findElement(By.cssSelector("img[alt='Fronte Della Carta']")).getAttribute("outerHTML"));
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
             WebElement cardImage = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("img[alt='Fronte Della Carta']")));
-
             ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", cardImage);
             ((JavascriptExecutor) driver).executeScript("window.scrollBy(0,-1000);");
             try{
                 Thread.sleep(1000);
-            }catch (InterruptedException e){
-
-            }
+            }catch (InterruptedException e){}
             cardImage.click();
-
-            System.out.println(driver.getCurrentUrl());
+            String url = driver.getCurrentUrl();
+            System.out.println(extractCid(url));
         }finally {
             driver.quit();
         }
