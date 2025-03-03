8df6c4ec4542adc76601d147b7fcac66299f2ace
RickyMandich <ricky.mandich@gmail.com>
2025-03-03 21:48:30 +0100

    aggiornamento 2025 03 03 21:48

--- src/main/java/com/example/webscraping/Test.java
+++ src/main/java/com/example/webscraping/Test.java
@@ -2,18 +2,18 @@ package com.example.webscraping;
 
 public class Test {
     public static void main(String[] args) {
-        ThreadMessage tm = new ThreadMessage();
-        tm.start();
-        tm.addMessage("Hello");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        System.out.println("inserisci il nome del primo giocatore");
+        String g1 = new java.util.Scanner(System.in).nextLine();
+        System.out.println("inserisci il nome del secondo giocatore");
+        String g2 = new java.util.Scanner(System.in).nextLine();
+        java.util.List<String> mazzi = new java.util.ArrayList<>();
+        String line;
+        System.out.println("inserisci il nome del primo mazzo");
+        while(!(line = new java.util.Scanner(System.in).nextLine()).isEmpty()) {
+            mazzi.add(line);
+            System.out.println("inserisci il nome del prossimo mazzo");
         }
-        tm.addMessage("World");
-        tm.finish();
-        try {
-            tm.join();
-        }catch (InterruptedException ignore){}
+        System.out.println(g1 + ":\t" + mazzi.remove(new java.util.Random().nextInt(mazzi.size())));
+        System.out.println(g2 + ":\t" + mazzi.remove(new java.util.Random().nextInt(mazzi.size())));
     }
 }
